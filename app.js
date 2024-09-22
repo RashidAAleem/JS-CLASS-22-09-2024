c93d4e0ba8a283b3abfe922e6c51b9c2b6083c6a
Rana Rashid <124416215+RashidAAleem@users.noreply.github.com>
2024-09-22 06:53:12 +0000

    update

--- app.js
+++ app.js
@@ -21,26 +21,27 @@
 // document.write("<b>sliced: </b> "+ firstChar + remainingChar + "<br>")
 
 
+//ascending order
 
-// var number = [24, 53, 78, 91, 12, 152, 125];
-// for (var i = 0; i < number.length; i++) {
-// for (var j = 0; j < number.length; j++) {
-//     if (number[j]> number[j+1]) {
-//         [number[j], number[j+1]] = [number[j+1], number [j]]
+var number = [24, 53, 78, 91, 12, 152, 125];
+for (var i = 0; i < number.length; i++) {
+for (var j = 0; j < number.length; j++) {
+    if (number[j]> number[j+1]) {
+        [number[j], number[j+1]] = [number[j+1], number [j]]
         
-//     }
+    }
     
-// }
+}
     
 
     
     
-// }
-// document.write(number); 
+}
+document.write(number); 
 
 
 // descending order
-var number1 = ["24", 53, 78, 91, 12, 152, 125];
+var number1 = [24, 53, 78, 91, 12, 152, 125];
 for (var i = 0; i < number1.length; i++) {
 for (var j = 0; j < number1.length; j++) {
     if (number1[j]< number1[j+1]) {
